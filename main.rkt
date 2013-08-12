d73ee2dccfe347b6515fb4c5bdb674f23ed79a8c
Spencer Florence <florence@ccs.neu.edu>
2013-08-12 18:52:25 -0400

    construction page

---
+++ main.rkt
@@ -0,0 +1,13 @@
+#lang racket
+(require web-server/servlet
+         web-server/servlet-env)
+ 
+(define (start req)
+  (response/xexpr
+   `(html (head (title "Libertees: Coming Soon"))
+          (body (p "Under Construction")))))
+ 
+(serve/servlet start
+               #:listen-ip #f
+               #:servlet-regexp #rx""
+               #:port 5674)
