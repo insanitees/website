fa08fc4acc5b23b34e694db136fb8fff3a70960b
Spencer Florence <florence@ccs.neu.edu>
2013-08-13 16:23:09 +0000

    better PID tracking

--- main.rkt
+++ main.rkt
@@ -1,12 +1,13 @@
 #lang racket
 (require web-server/servlet
          web-server/servlet-env)
- 
+
 (define (start req)
   (response/xexpr
-   `(html (head (title "Libertees: Coming Soon"))
+   `(html (head (title "Libertees: Coming Soon")
+                (style ((type "text/css")) "p {text-align: center; font-size: 3em;}"))
           (body (p "Under Construction")))))
- 
+
 (serve/servlet start
                #:listen-ip #f
                #:servlet-regexp #rx""
