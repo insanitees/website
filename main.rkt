d80d847d9425afe581a8c4bcdd6d5c8c668299a3
Spencer Florence <florence@ccs.neu.edu>
2013-08-14 16:36:04 -0400

    Revert "added a mutex to the users map," turns out its not needed
    This reverts commit 3063ba7d609e100affce12f0cbaf29884f988ff1.

--- main.rkt
+++ main.rkt
@@ -26,10 +26,9 @@
         (export servlet^))
       (values path serve)))
   (define users (make-hash))
-  (define user-mutex (make-semaphore 1))
   (lambda (req)
     (define uri (path->string (url->path (request-uri req))))
-    (define user (get-user! req user-mutex users))
+    (define user (get-user! req users))
     (or 
      (for/first ([(rx serve) servlet-map] #:when (regexp-match? rx uri))
        (serve req (make-page-maker user) user))
@@ -43,9 +42,9 @@
                 (style ((type "text/css")) "p {text-align: center; font-size: 3em;}"))
           ,body)))
 
-;; request? semaphore [make-hash string? user?] -> user?
+;; request? [make-hash string? user?] -> user?
 ;; the the user for a key. make and add one if it doesn't exist
-(define (get-user! req sema users)
+(define (get-user! req users)
   (define (make-user!)
     (define usr (user (string->bytes/utf-8 (~a (gensym 's)))))
     (hash-set! users (user-session-id usr) usr)
@@ -60,8 +59,7 @@
       (bytes->string/utf-8
        (binding:form-value b)))]
       [_ #f]))
-  (call-with-semaphore sema
-                       (thunk (if key (hash-ref users key make-user!) (make-user!)))))
+  (if key (hash-ref users key make-user!) (make-user!)))
 
 ;; test servlet^
 (define-unit under-construction@
