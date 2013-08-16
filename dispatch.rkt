b14801a16ec05f16142dbdf0a26e67185538fa20
Spencer Florence <florence@ccs.neu.edu>
2013-08-15 22:55:27 -0400

    session timeouts completed

--- dispatch.rkt
+++ dispatch.rkt
@@ -14,7 +14,8 @@ this module provides bindings to launch the server
 (require "shared.rkt")
 
 (define SESSION-KEY "session")
-(struct in:user user (time))
+(define SESSION-LIFETIME 60 #;(* 60 60 24)) ;; a day in seconds
+(struct in:user user ([time #:mutable]) #:transparent)
 
 ;; run the server
 (define (launch . units)
@@ -36,11 +37,13 @@ this module provides bindings to launch the server
 (define (dispatch servlets)
   (define servlet-map (make-servlet-map servlets))
   (define users (make-hash))
+  (launch-session-cleaning users)
   (lambda (req)
     (define path (path->string (url->path (request-uri req))))
     (or 
      (for/first ([(rx serve) servlet-map] #:when (regexp-match? rx path))
        (define user (get-user! req users))
+       (displayln users)
        (serve req (make-page-maker user) user))
      (raise (exn:dispatcher)))))
 
@@ -52,6 +55,18 @@ this module provides bindings to launch the server
       (import)
       (export servlet^))
     (values path serve)))
+
+;; [make-hash string? in:user?] -> thread?
+;; launch the thread to clean out old users
+(define (launch-session-cleaning users)
+  (thread 
+   (thunk
+    (let loop ()
+      (sync/timeout 60 never-evt)
+      (for-each (lambda (u) (and (> (- (current-seconds) (in:user-time u)) SESSION-LIFETIME)
+                                 (hash-remove! users (user-session-id u))))
+        (hash-values users))
+      (loop)))))
 
 ;; make the function that builds a standard header for a page
 ;; user? -> make-page/c
@@ -66,11 +81,13 @@ this module provides bindings to launch the server
 ;; the the user for a key. make and add one if it doesn't exist
 (define (get-user! req users)
   (define (make-user!)
-    (define usr (in:user (~a (gensym 's)) (current-milliseconds)))
+    (define usr (in:user (~a (gensym 's)) (current-seconds)))
     (hash-set! users (user-session-id usr) usr)
     usr)
   (define key 
     (client-cookie-value
      (findf (lambda (c) (string=? SESSION-KEY (client-cookie-name c)))
             (request-cookies req))))
-  (if key (hash-ref users key make-user!) (make-user!)))
+  (define usr (if key (hash-ref users key make-user!) (make-user!)))
+  (set-in:user-time! usr (current-seconds))
+  usr)
