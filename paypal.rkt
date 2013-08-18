55cfdb9a08891a5666433e290bf535e571716e50
Spencer Florence <florence@ccs.neu.edu>
2013-08-18 11:32:13 -0400

    starting on paypal IPN callback endpoint

--- paypal.rkt
+++ paypal.rkt
@@ -12,13 +12,8 @@ This module provides values to handle paypal integration
   [add-to-cart-button make-paypal-button/c]))
 
 (require "shared.rkt" "paypal-credentials.rkt" xml)
-
-(define-unit paypal-callback@
-  (import)
-  (export servlet^)
-  (define path #rx"/?paypal/?")
-  (define (serve req? make-page)
-    (make-page `(body ()))))
+(require web-server/http/request-structs)
+(require net/url)
 
 (define make-paypal-button/c (-> string? (and/c number? real? (curry < 0)) xexpr?))
 (define ((make-paypal-button url button-name) product-name amount)
@@ -32,3 +27,46 @@ This module provides values to handle paypal integration
 
 (define add-to-cart-button 
   (make-paypal-button "paypal-button-minicart.min.js?merchant=" "cart"))
+
+(define-unit paypal-callback@
+  (import)
+  (export servlet^)
+  (define path #rx"/?paypal/?")
+  
+  ;; see https://developer.paypal.com/webapps/developer/docs/classic/ipn/integration-guide/IPNIntro/
+  ;; for callback information
+  (define (serve req? make-page)
+    (make-page `(body ())))
+  
+  #;(define (get-reciever-information req)
+    )
+  #;(define (get-transaction-information req)
+    )
+  #;(define (get-buyer-info req)
+    )
+  #;(define (get-address req)
+    )
+  #;(define (get-payment-info req)
+    )
+  (define (verity req)
+    ;; todo: validate https certs
+    (define req-uri (request-uri req))
+    (define validate-url
+      (struct-copy url req-uri
+                   [query (cons (cons 'cmd "_notify-validate")
+                                (url-query req-uri))]
+                   [path (list (path/param "cgi-bin" empty) (path/param "webscr" empty))]
+                   [host "www.sandbox.paypal.com"]
+                   [scheme "https"]))
+    (define resp (port->string (get-pure-port validate-url)))
+    (and (equal? resp "VALID")
+         (raise (exn:insanitees:bad-paypal-IPN #;todo))))
+  
+  ;; request bytes (-> bytes A) -> (maybe A)
+  (define (load-binding req key convert)
+    (define raw-binding
+      (bindings-assq key (request-bindings/raw req)))
+    (match key 
+      [(? binding:form? b)
+       (convert (binding:form-value b))]
+      [_ #f])))
